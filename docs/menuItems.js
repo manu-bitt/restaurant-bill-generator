775641e3cb439afc53d189d5f8475f8ad0a290d1
Manu <manupal8954@gmail.com>
2025-04-06 15:36:10 +0530

    Remove all default menu items

--- docs/menuItems.js
+++ docs/menuItems.js
@@ -1,5 +1,2 @@
 // Menu items data with name and price
-const menuItems = [
-    // Default menu items removed
-    // Add your own menu items or use the "Manage Menu" button
-]; 
+const menuItems = []; 
